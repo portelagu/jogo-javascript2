a42dbb537ee432a79174060e5b1f5569f85b91e5
Gustavo <ggomesportela26@gmail.com>
2024-02-29 16:47:42 -0300

    commit final

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 500');
 }
 
 exibirMensagemInicial();
